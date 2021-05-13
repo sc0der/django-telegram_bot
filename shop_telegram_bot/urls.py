273f341bcb8c24534f70954aa80466140af5170b
sc0der <siriusproger@gmail.com>
2021-05-13 13:34:31 +0200

    routes updated

--- shop_telegram_bot/urls.py
+++ shop_telegram_bot/urls.py
@@ -16,7 +16,6 @@ from big_shop.views import (
 )
 
 router = routers.DefaultRouter()
-
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
