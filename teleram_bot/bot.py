112b39da977d8ca912d029f1e072af92c77ff470
Sirius Sufiew <44986984+sc0der@users.noreply.github.com>
2021-05-29 03:32:48 +0500

    added changes to bot

--- teleram_bot/bot.py
+++ teleram_bot/bot.py
@@ -8,9 +8,10 @@ from aiogram.types.input_media import InputMediaPhoto
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-# init categories
+# init categories   
 fetchCategory = FetchCategory(category_urls)
 fetchProduct = FetchProduct(products_urls)
+
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Привет!", reply_markup=kb.category_menu)
@@ -31,25 +32,41 @@ async def process_start_command(message: types.Message):
     commands = [cat.name for cat in fetchCategory.get_category_list()]
     category_dict = {cat.name:cat.slug for cat in fetchCategory.get_category_list()}
     if message.text in commands:
-        products = fetchCategory.get_products_to_category(category_dict[message.text])
-        photos = []
-
-        
-
-
-        # http://localhost:8000//photo_archive/products/n21ooj7vyeyvmfule7bjxxy0yqutjd.png
+        products = fetchCategory.get_products_to_category(
+                category_dict[message.text]
+            )
+        # http://localhost:8000/photo_archive/products/n21ooj7vyeyvmfule7bjxxy0yqutjd.png
 
         # for prod in products:
         prod = products[0]
         photo = fetchProduct.get_photo_by_id(prod.slug)
+        photos = []
+        # for foto in photo:
+        #     photos.append(
+        #         InputMediaPhoto(
+        #             "http://localhost:8000/photo_archive/products/n21ooj7vyeyvmfule7bjxxy0yqutjd.png",
+        #             foto['photo'],
+        #             ''
+        #         )
+        #     )
+
+        photos.append(
+                InputMediaPhoto(
+                    "https://cdn.wallpapersafari.com/93/32/64OhnG.jpg",
+                    'photo'
+                )
+            )
+
         print("prod: ", prod.name)
         print("prod_slg: ", prod.slug)
         print("prod_price: ", prod.price)
+        # print("products: ", photos[0])
+
+        # print('photo: ', photos[1])
         # photos.append(host + photo[0]['photo'])
-        photos.append(InputMediaPhoto('https://i.morioh.com/2020/03/07/6c7bae688a0e.jpg',  prod.slug ))
-        paginator = kb.MyPaginator(len(products))
+        # paginator = kb.MyPaginator(len(products))
         await bot.send_media_group(message.from_user.id, photos)
-        await message.reply(photos, reply_markup=paginator.markup)
+        # await message.reply(prod.name, reply_markup=paginator.markup)
     else:
         print('nest')
         await message.reply('kuku: ' + message.text, reply_markup=kb.category_menu)
